d5f5076fb30459b048f7f494892d68f90f99273b
silentorb <christopherwjohnson@gmail.com>
2012-03-29 20:39:09 -0600

    Added a simple event system. Fixed some bugs. Simplified the Block system. Cleaned out several uneeded functions.

---
+++ test/test.js
@@ -0,0 +1,11 @@
+
+$(document).ready(function(){
+    
+    // There will be testing here.
+  test("a basic test example", function() {
+    ok( true, "this test is fine" );
+    var value = "hello";
+    equal( value, "hello", "We expect value to be hello" );
+  });
+
+});
