e70481a7fd4465eab0c36292afdc8b9f63a9bb38
silentorb <christopherwjohnson@gmail.com>
2012-04-10 09:09:29 -0600

    More unit testing and implemented a form of dynamic block loading.

--- test/bloom-test.js
+++ test/bloom-test.js
@@ -1,19 +1,26 @@
-Block.library['test_div'] = new Block('div', '<div></div>');
+Block.library['test_div'] = new Block('test_div', '<div></div>');
+Block.library['test_list'] = new Block('test_list', '<ul></ul>');
 
 var Test_Element = Element.sub_class('Test_Element', {
   block: 'test_div'
 });
 
+var Test_List = List.sub_class('Test_List', {
+  block: 'test_list',
+  remote_list: 'nodes',
+  item_type: Meta_Object
+});
+
 $(function(){
      
-  test("Element.properties.initialize", function() {    
+  test("Element.initialize", function() {    
     var virtual_element = Element.create_without_initializing();
     Element.properties.initialize.apply(virtual_element, ['<div></div>']);
     ok(typeof virtual_element.element == 'object', "Element has element");
 
   });
   
-  test("Element.properties.render", function() {
+  test("Element.render", function() {
     var virtual_element = Element.create_without_initializing();
     virtual_element.block = 'test_div';
     virtual_element.render();
@@ -25,14 +32,40 @@ $(function(){
       function_called = true;
     });
     
-    ok(function_called, "element.render called the onload function that was passed to it");
+    ok(function_called, "Element.render called the onload function that was passed to it");
   });
 
-  test("Element.properties.create", function() {
-    var control = Test_Element.create();
+  test("Element.create", function() {
+    var control;
+      
+    control = Test_Element.create();
     ok(typeof control.element === 'object', "Test_Element has element");
-    
-    var control = Element.create('<div></div>');    
+     
+    var function_called = false;
+    control = Test_Element.create(function() {
+      function_called = true;
+    });    
+    ok(typeof control.element === 'object', "Test_Element has element");
+    ok(function_called, "Element.render called the onload function that was passed to it");
+
+    control = Element.create('<div></div>');    
     ok(typeof control.element === 'object', "Element has element");
-  });  
+  });
+  
+  test("List.on_update", function() {   
+    var data = {
+      nodes: []
+    };
+    data.nodes.push(Test.create_node());
+    
+    var function_called = false;
+    Test_List.create(function(list){
+      list.on_update(data);
+      ok(list.children.length > 0, 'list has children')
+      ok(list.element.length > 0, 'list has element')
+      function_called = true;
+    });
+
+    ok(function_called, "List.create called the onload function that was passed to it");  
+  });
 });
